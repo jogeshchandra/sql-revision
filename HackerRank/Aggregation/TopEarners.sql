97af6ccb18ea56784045b60306c6032b05aa88bf
Your Name <you@example.com>
2025-12-16 21:29:48 +0530

    E

---
+++ HackerRank/Aggregation/TopEarners.sql
@@ -0,0 +1,31 @@
+ 🔗 Problem:
+https://www.hackerrank.com/challenges/earnings-of-employees/problem?isFullScreen=true
+
+📌 Title:
+<Top Earner>
+
+📌 Platform:
+<HackerRank >
+
+📌 Concept:
+<Select /Aggregations >
+
+📌 Difficulty:
+<Easy >
+
+
+⚠️ Things I learned:
+- <Key skill / trick>
+- <Optimization approach>
+- <Anything new>
+
+🧠 Mistakes to avoid next time:
+im confused between when i can use the collum allias from a subquery and all
+
+Quick Mental Model (Remember This)
+Think like this:
+FROM → WHERE → GROUP → HAVING → SELECT (alias created) → ORDER BY → LIMIT
+So:
+Anything before SELECT cannot see aliases
+Anything after SELECT can
+
